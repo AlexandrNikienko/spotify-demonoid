5fc6fa4c37f49f3fbc44230c4a7e0b158ac027c1
Oleksandr Nikiienko <oleksandr.nikiienko@innervate.com>
2025-10-09 18:54:58 +0300

    redirect uri fix

--- src/script.ts
+++ src/script.ts
@@ -1,9 +1,15 @@
 const clientId = "33c276b6719a4a64b6cc3d0cb518e727";
 const params = new URLSearchParams(window.location.search);
 const code = params.get("code");
-const redirect_uri = window.location.hostname === "127.0.0.1"
-    ? "http://127.0.0.1:5173/"
-    : "https://spotify-demonoid.netlify.app/";
+let redirect_uri: string;
+
+if (window.location.hostname === "127.0.0.1") {
+    console.log("Running on localhost");
+    redirect_uri = "http://127.0.0.1:5173/";
+} else {
+    console.log("Running in production");
+    redirect_uri = "https://spotify-demonoid.netlify.app/";
+}
 
 (async () => {
     let profile = await checkToken(localStorage.getItem("access_token"));
